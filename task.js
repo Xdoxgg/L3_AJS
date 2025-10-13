a6e589562f934ab8750fb4169617f41bffc01a47
Xdoxgg <m.zhuk007@gmail.com>
2025-10-13 13:28:34 +0300

    finish lab

--- task.js
+++ task.js
@@ -1,13 +1,26 @@
-
 function callbackTest(response) {
     console.log(response);
 }
 
 function printPosts(posts) {
 
-    posts = posts.sort(function(a, b) {return b.title.length - a.title.length});
+    posts = posts.sort(function (a, b) {
+        return b.title.length - a.title.length
+    });
     posts.forEach(post => {
         console.log(post);
+    })
+}
+
+function printTodos(todos) {
+    let newTodos = []
+    todos.forEach(todo => {
+        if (todo.completed == 'falsy') {
+            newTodos.push(todo);
+        }
+    })
+    newTodos.forEach(newTodo => {
+        console.log(newTodo);
     })
 }
 
@@ -15,24 +28,26 @@ function printComments(comments, users) {
     comments.forEach(comment => {
         let userName = ""
         users.forEach(user => {
-            if(user.id == comment.user_id) {
+            if (user.id == comment.user_id) {
                 comment.username = user.username;
             }
         })
     })
-    comments = comments.sort(function(a, b) {a.username.localeCompare(b.username)});
+    comments = comments.sort(function (a, b) {
+        a.username.localeCompare(b.username)
+    });
     console.log(comments);
 }
 
 async function getPosts(callback = null) {
     let response = await fetch("https://my-json-server.typicode.com/Xdoxgg/testDbRepo/posts");
     let data = await response.json();
-    if (callback) callback(data);
+    if (callback) callback(data);// мб тут стоило возвращать callback, но я хз
     return data;
 }
 
 
-async function getComments(callback  = null) {
+async function getComments(callback = null) {
     let response = await fetch("https://my-json-server.typicode.com/Xdoxgg/testDbRepo/comments");
     let data = await response.json();
     let users = await getUsers();
@@ -44,20 +59,52 @@ async function getComments(callback  = null) {
 async function getUsers(callback = null) {
     let response = await fetch("https://my-json-server.typicode.com/Xdoxgg/testDbRepo/users");
     let data = await response.json();
-    return data;
     if (callback) callback(data);
     return data;
 
 }
 
 
+async function updateUsers(callback = null) {
+    let users = null;
+    try {
+        users = await getUsers();
+        users.forEach(user => {
+            delete user.empty;
+        })
+    } catch (err) {
+        console.log(err);
+    }
+    return users;
+    // let response = fetch("https://my-json-server.typicode.com/Xdoxgg/testDbRepo/users", {
+    //     method: "POST",
+    //     headers: { "Content-Type": "application/json" },
+    //     body: JSON.stringify(users)
+    // }).then(response => {
+    //     if (!response.ok) {
+    //         throw new Error('Ошибка сети: ' + response.status);
+    //     }
+    //     return response.json();
+    // })
+    //     .then(data => {
+    //         console.log('Ответ сервера:', data);
+    //     })
+    //     .catch(error => {
+    //         console.error('Ошибка запроса:', error);
+    //     });
+}
 
-// await getPosts(printPosts);
-await getComments(printComments);
-// await getUsers(callbackTest);
+async function getTodos(callback = null) {
+    let response = await fetch("https://my-json-server.typicode.com/Xdoxgg/testDbRepo/todos");
+    let data = await response.json();
+    if (callback) callback(data);
+    return data;
+}
 
-
-
+// await getPosts(printPosts); // 1
+// await getComments(printComments); // 2
+// callbackTest(await updateUsers(callbackTest)) //3
+await getTodos(printTodos);
 
 
 
